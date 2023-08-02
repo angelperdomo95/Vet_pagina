e97fb42808747e16d6c2ee62d36ad612c21fec3d
angelperdomo95 <mi_lo2009@hotmail.es>
2023-08-02 01:59:18 -0500

    commit para agregar los formatos services reviews y footer para poder editar y configurar segun el diseño de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Header from "./components/1.header/Header"
 import Hero from "./components/2.hero/Hero"
 import Clients from "./components/3.clients/Clients"
 import Works from "./components/4.works/Works"
+import Services from "./components/5.services/Services"
+import Reviews from "./components/6.Reviews/Reviews"
+import Footer from "./components/7.footer/Footer"
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
     <Hero />
     <Clients/>
     <Works/>
+    <Services/>
+    <Reviews/>
+    <Footer/>
     </div>
   )
 }
