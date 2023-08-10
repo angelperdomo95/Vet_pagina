d795967195c3a18e6c9b1350f8badc4236824e86
angelperdomo95 <mi_lo2009@hotmail.es>
2023-08-10 17:06:22 -0500

    actualizacion para page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://angelperdomo95.github.io/Vet_pagina/",
+  base:"https://angelperdomo95.github.io/vet_page/",
 })
